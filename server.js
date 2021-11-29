aa1b27a9bb0cfb9f302e7f861ef2a69e1e74bc80
Moradi1412 <moradi.javid@gmail.com>
2021-11-28 23:09:33 -0800

    create server.js and first route

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3003, () => {
+    console.log(`API server now on port 3003!`);
+  });
+
+
+  app.get('/', (req, res) => {
+    res.send('Hello!');
+  });

829650e388884fb495e30a81407fa1233a124e20
Moradi1412 <moradi.javid@gmail.com>
2021-11-28 23:15:23 -0800

    connect to db.json

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const express = require('express');
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+var notesdb = require("./db/db.json")
 
-const app = express();
+var app = express();
+var PORT = process.env.PORT || 8080;
 
-app.listen(3003, () => {
-    console.log(`API server now on port 3003!`);
-  });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
   app.get('/', (req, res) => {
-    res.send('Hello!');
+    res.send(notesdb);
+  });
+
+
+//   app.get("*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "public/index.html"));
+// });
+
+
+app.listen(3003, () => {
+    console.log(`API server now on port 3003!`);
   });

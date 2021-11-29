feedb3239a31a06a04ac857ab0857a660082099e
Moradi1412 <moradi.javid@gmail.com>
2021-11-28 23:50:05 -0800

    bug

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 var notesdb = require("./db/db.json")
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3003;
 
 
 app.use(express.urlencoded({ extended: true }));
